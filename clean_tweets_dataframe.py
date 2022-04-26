f6e7bc251010951ae8ad203fd132d34ee2e905cc
Kevin <shyakakevin1@gmail.com>
2022-04-26 23:07:09 +0200

    file changed

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -20,7 +20,8 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-        
+        self.df = self.df.drop_duplicates().drop_duplicates(subset='original_text')
+
         ---
         
         return df
@@ -29,7 +30,7 @@ class Clean_Tweets:
         convert column to datetime
         """
         ----
-        
+        self.df['created_at'] = pd.to_datetime(self.df['created_at'], errors='coerce')
         ----
         
         df = df[df['created_at'] >= '2020-12-31' ]
